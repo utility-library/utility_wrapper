3d40036a7112609599a7e5651655220d72aa42f5
Xenos.exe <arcobalenosixsiegea@gmail.com>
2023-02-25 01:00:58 +0100

    refactor+feat(shared/wrapper): changed wrapper load system and added event hooks

--- shared/wrapper.lua
+++ shared/wrapper.lua
@@ -1,5 +1,6 @@
 _exports = exports
 local metaExports = getmetatable(exports)
+local eventHooks = {}
 
 local utilityWrapper = exports["utility_wrapper"]
 Config = utilityWrapper:getConfig()
@@ -8,24 +9,20 @@ function getObject(from)
     local to = Config.Framework
 
     if from == to then -- if the resource requires the same framework load it as usual
-        print("loading normal framework ("..from..")")
+        print("loading framework ("..from..")")
         local method = utilityWrapper:getMethodName(from)
+
+        -- using the old exports variable to call the original framework export 
         return _exports[Config.FrameworkResourceName][method]()
 
-    else -- else get the wrapper file and load it
+    else -- else get the wrapper
         print("loading wrapper from "..from.." to "..to)
-
-        local wrapper = utilityWrapper:getWrapperFile(from, to)
-        local loaded = load(wrapper)
-
-        local success, result = pcall(loaded)
-
-        if not success then
-            print("^1WRAPPER ERROR: "..result.."^0")
-            print("^1TRY RUNNING THE WRAPPER DIRECTLY TO GET A BETTER STACK TRACEBACK^0")
-        else
-            return result
-        end
+        -- the wrapper is pre-loaded for ease of fixing bugs (stack traceback from the load function was quite useless)
+        local wrapper = utilityWrapper:getWrapper(from, to)
+        local loaded = wrapper()
+        
+        eventHooks = loaded.events
+        return loaded.object
     end
 end
 
@@ -55,3 +52,41 @@ exports = setmetatable({}, {
     __newindex = metaExports.__newindex,
     __call = metaExports.__call,
 })
+
+
+-- Event hooks
+local HookEvent = function(func, cb, eventHook)
+    if type(eventHook) == "string" then
+        func(eventHook, cb)
+    else
+        if eventHook.faker then
+            eventHook.faker(cb)
+        else
+            func(eventHook.name, function(...)
+                eventHook.hook(cb, ...)
+            end)
+        end
+    end
+end
+
+local _RegisterNetEvent = RegisterNetEvent
+RegisterNetEvent = function(eventName, cb)
+    if cb then
+        if eventHooks[eventName] then
+            HookEvent(_RegisterNetEvent, cb, eventHooks[eventName])
+        else
+            _RegisterNetEvent(eventName, cb)
+        end
+    else
+        _RegisterNetEvent(eventName)
+    end
+end
+
+local _AddEventHandler = AddEventHandler
+AddEventHandler = function(eventName, cb)
+    if eventHooks[eventName] then
+        HookEvent(_AddEventHandler, cb, eventHooks[eventName])
+    else
+        _AddEventHandler(eventName, cb)
+    end
+end
